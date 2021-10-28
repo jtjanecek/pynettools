e302810678742c2f77f368cd924b6d5f55fc7f2d
John Janecek <janecektyler@gmail.com>
2021-10-28 12:54:33 -0600

    Update

--- pynettools/utils.py
+++ pynettools/utils.py
@@ -21,7 +21,6 @@ def port_open(ip=None, port=None, timeout=2):
 
 	logger.debug(f"{name}: Checking port_open on: {ip}: {port} ...")
 	try:
-		raise Exception
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.settimeout(timeout) 
 		result = sock.connect_ex((ip,port))
