9cf4dae52fd598b853f71d632655dd7e4e6441c2
John Janecek <janecektyler@gmail.com>
2021-10-29 11:39:13 -0600

    Add ping command

--- pynettools/utils.py
+++ pynettools/utils.py
@@ -1,4 +1,5 @@
 import socket
+import platform    # For getting the operating system name
 import subprocess
 from subprocess import PIPE
 from subprocess import check_output
@@ -40,4 +41,19 @@ def execute(command):
 	out = out.decode().strip()
 	logger.debug(f"{name}: Result: {out}")
 	return out
+
+def ping(host):
+	"""
+	Returns True if host (str) responds to a ping request.
+	Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+	"""
+
+	# Option for the number of packets as a function of
+	param = '-n' if platform.system().lower()=='windows' else '-c'
+
+	# Building the command. Ex: "ping -c 1 google.com"
+	command = ['ping', param, '1', host]
+
+	return subprocess.call(command) == 0
+
 
